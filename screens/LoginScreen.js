e00e2d768de00a9acdcd6c292693fc8538acdb21
leonardoaranhabyjus <97252824+leonardoaranhabyjus@users.noreply.github.com>
2022-02-17 17:18:44 -0300

    init

---
+++ screens/LoginScreen.js
@@ -0,0 +1,224 @@
+import React, { Component } from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  SafeAreaView,
+  Platform,
+  StatusBar,
+  Image,
+  Dimensions
+} from "react-native";
+import { TouchableOpacity } from "react-native-gesture-handler";
+import { RFValue } from "react-native-responsive-fontsize";
+import * as Google from "expo-google-app-auth";
+import firebase from "firebase";
+
+import AppLoading from "expo-app-loading";
+import * as Font from "expo-font";
+
+let customFonts = {
+  "Bubblegum-Sans": require("../assets/fonts/BubblegumSans-Regular.ttf")
+};
+
+export default class LoginScreen extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      fontsLoaded: false
+    };
+  }
+
+  async _loadFontsAsync() {
+    await Font.loadAsync(customFonts);
+    this.setState({ fontsLoaded: true });
+  }
+
+  componentDidMount() {
+    this._loadFontsAsync();
+  }
+
+  isUserEqual = (googleUser, firebaseUser) => {
+    if (firebaseUser) {
+      var providerData = firebaseUser.providerData;
+      for (var i = 0; i < providerData.length; i++) {
+        if (
+          providerData[i].providerId ===
+          firebase.auth.GoogleAuthProvider.PROVIDER_ID &&
+          providerData[i].uid === googleUser.getBasicProfile().getId()
+        ) {
+          // Não precisamos reautenticar a conexão do Firebase.
+          return true;
+        }
+      }
+    }
+    return false;
+  };
+
+  onSignIn = googleUser => {
+    // Precisamos registrar um Observer (observador) no Firebase Auth para garantir que a autenticação seja inicializada.
+    var unsubscribe = firebase.auth().onAuthStateChanged(firebaseUser => {
+      unsubscribe();
+      // Verifique se já estamos conectados ao Firebase com o usuário correto.
+      if (!this.isUserEqual(googleUser, firebaseUser)) {
+        // Crie uma credencial do Firebase com o token de ID do Google.
+        var credential = firebase.auth.GoogleAuthProvider.credential(
+          googleUser.idToken,
+          googleUser.accessToken
+        );
+
+        // Login com a credencial do usuário do Google.
+        firebase
+          .auth()
+          .signInWithCredential(credential)
+          .then(function (result) {
+            if (result.additionalUserInfo.isNewUser) {
+              firebase
+                .database()
+                .ref("/users/" + result.user.uid)
+                .set({
+                  gmail: result.user.email,
+                  profile_picture: result.additionalUserInfo.profile.picture,
+                  locale: result.additionalUserInfo.profile.locale,
+                  first_name: result.additionalUserInfo.profile.given_name,
+                  last_name: result.additionalUserInfo.profile.family_name,
+                  current_theme: "dark"
+                })
+                .then(function (snapshot) { });
+            }
+          })
+          .catch(error => {
+            // Trate os erros aqui.
+            var errorCode = error.code;
+            var errorMessage = error.message;
+            // O e-mail da conta do usuário que foi usada.
+            var email = error.email;
+            // O tipo do firebase.auth.AuthCredential que foi usado.
+            var credential = error.credential;
+            // ...
+          });
+      } else {
+        console.log("Usuário já conectado ao Firebase.");
+      }
+    });
+  };
+
+  signInWithGoogleAsync = async () => {
+    try {
+      const result = await Google.logInAsync({
+        behaviour: "web",
+        androidClientId:
+          "72696421845-lqe44rrjuiggsegp1uv4gklv34tvl3gc.apps.googleusercontent.com",
+        iosClientId:
+          "72696421845-osrvc36bjie4264j4c0812sp5a2egqhj.apps.googleusercontent.com",
+        scopes: ["profile", "email"]
+      });
+
+      if (result.type === "success") {
+        this.onSignIn(result);
+        return result.accessToken;
+      } else {
+        return { cancelled: true };
+      }
+    } catch (e) {
+      console.log(e.message);
+      return { error: true };
+    }
+  };
+
+  render() {
+    if (!this.state.fontsLoaded) {
+      return <AppLoading />;
+    } else {
+      return (
+        <View style={styles.container}>
+          <SafeAreaView style={styles.droidSafeArea} />
+          <View style={styles.appTitle}>
+            <Image
+              source={require("../assets/logo.png")}
+              style={styles.appIcon}
+            ></Image>
+            <Text style={styles.appTitleText}>{`App Narração de\nHistórias`}</Text>
+          </View>
+          <View style={styles.buttonContainer}>
+            <TouchableOpacity
+              style={styles.button}
+              onPress={() => this.signInWithGoogleAsync()}
+            >
+              <Image
+                source={require("../assets/google_icon.png")}
+                style={styles.googleIcon}
+              ></Image>
+              <Text style={styles.googleText}>Login com o Google</Text>
+            </TouchableOpacity>
+          </View>
+          <View style={styles.cloudContainer}>
+            <Image
+              source={require("../assets/cloud.png")}
+              style={styles.cloudImage}
+            ></Image>
+          </View>
+        </View>
+      );
+    }
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#15193c"
+  },
+  droidSafeArea: {
+    marginTop: Platform.OS === "android" ? StatusBar.currentHeight : RFValue(35)
+  },
+  appTitle: {
+    flex: 0.4,
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  appIcon: {
+    width: RFValue(130),
+    height: RFValue(130),
+    resizeMode: "contain"
+  },
+  appTitleText: {
+    color: "white",
+    textAlign: "center",
+    fontSize: RFValue(40),
+    fontFamily: "Bubblegum-Sans"
+  },
+  buttonContainer: {
+    flex: 0.3,
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  button: {
+    width: RFValue(250),
+    height: RFValue(50),
+    flexDirection: "row",
+    justifyContent: "space-evenly",
+    alignItems: "center",
+    borderRadius: RFValue(30),
+    backgroundColor: "white"
+  },
+  googleIcon: {
+    width: RFValue(30),
+    height: RFValue(30),
+    resizeMode: "contain"
+  },
+  googleText: {
+    color: "black",
+    fontSize: RFValue(20),
+    fontFamily: "Bubblegum-Sans"
+  },
+  cloudContainer: {
+    flex: 0.3
+  },
+  cloudImage: {
+    position: "absolute",
+    width: "100%",
+    resizeMode: "contain",
+    bottom: RFValue(-5)
+  }
+});
