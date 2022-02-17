ed5f138d0bd5ba154df37a014e053e6fbef48462
leonardoaranhabyjus <97252824+leonardoaranhabyjus@users.noreply.github.com>
2022-02-17 17:39:39 -0300

    Correção Código

---
+++ config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyABQqZLp0v6RkM-Qj-_tFlczFVDCR0zvbE",
+    authDomain: "storytelling-app-60dbe.firebaseapp.com",
+    projectId: "storytelling-app-60dbe",
+    storageBucket: "storytelling-app-60dbe.appspot.com",
+    messagingSenderId: "817600991173",
+    appId: "1:817600991173:web:2656a4f561b0f1b74585ac"
+  };
